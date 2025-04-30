b83679ba3fb188cc7a5ce33bae84176e47fd15e8
MatsumotoKentaro <matsumoto.kentaro@gmail.com>
2025-04-30 15:40:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# pyxel_development
+pyxel開発環境
+
+## 開発環境構築
+
+uvをインストールした環境で以下を実行する
+
+```sh
+uv sync
+```
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import pyxel
+
+pyxel.init(160, 160, "Hello Pyxel")
+
+pyxel.text(10, 10, "Hello Pyxel!", 10)
+
+pyxel.show()
