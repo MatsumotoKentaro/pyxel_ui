c83fdc8169608a4a203f0241e59a4b780d7e466e
KentaroMatsumoto <matsumoto.kentaro@gmail.com>
2025-04-30 16:16:24 +0900

    Add Column

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyxel
 
-from mini_ui import Button, Label
+from mini_ui import Button, Column, Label
 
 pyxel.init(160, 160, "Hello Pyxel")
 
@@ -16,15 +16,24 @@ def update_text():
 
 button = Button("Hello, Button", update_text, 10, 50, 100, 16, 1, 2)
 
+menu_ui = Column(
+    64,
+    64,
+    spacing=12,
+    children=[
+        label,
+        button,
+    ],
+)
+
 
 def update():
-    button.update()
+    menu_ui.update()
 
 
 def draw():
     pyxel.cls(0)
-    label.draw()
-    button.draw()
+    menu_ui.draw()
 
 
 pyxel.run(update, draw)
--- mini_ui.py
+++ mini_ui.py
@@ -46,3 +46,29 @@ class Button(Widget):
     def draw(self):
         pyxel.rect(self.x, self.y, self.w, self.h, self.color_rect)
         pyxel.text(self.x + 4, self.y + 4, self.text, 0)
+
+
+# 縦に並べるレイアウト
+class Column(Widget):
+    def __init__(self, x, y, spacing=8, children=[], center=True):
+        self.x = x
+        self.y = y
+        self.spacing = spacing
+        self.children = children
+        self.center = center
+        self._layout()
+
+    def _layout(self):
+        total_height = len(self.children) * self.spacing
+        offset_y = self.y - total_height // 2 if self.center else self.y
+        for i, child in enumerate(self.children):
+            child.x = self.x - pyxel.FONT_WIDTH * len(getattr(child, "text", "")) // 2
+            child.y = offset_y + i * self.spacing
+
+    def update(self):
+        for child in self.children:
+            child.update()
+
+    def draw(self):
+        for child in self.children:
+            child.draw()
