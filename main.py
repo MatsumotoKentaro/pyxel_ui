fd80ea58c121ae977fba374504fff81d489330b2
MatsumotoKentaro <matsumoto.kentaro@gmail.com>
2025-04-30 21:35:32 +0900

    Update: README, main.py (#4)

--- main.py
+++ main.py
@@ -2,41 +2,39 @@ import pyxel
 
 from mini_ui import Button, Column, Label
 
-pyxel.init(160, 160, "Hello Pyxel")
-pyxel.mouse(True)
 
-count = 0
-label_text = f"click count:{count}"
-label = Label(label_text, color=7)
+class App:
+    def __init__(self):
+        pyxel.init(160, 160, "mini ui sample")
+        pyxel.mouse(True)
 
+        self.count = 0
+        self.label = Label(f"click count:{self.count}", color=7)
+        self.column = Column(
+            0,
+            0,
+            spacing=12,
+            children=[
+                Button("Click", self.update_text, center_x=True),
+                self.label,
+            ],
+            w=pyxel.width,
+            h=pyxel.height,
+            align="center",
+        )
 
-def update_text():
-    global count, label
-    count += 1
-    label.text = f"click count:{count}"
+        pyxel.run(self.update, self.draw)
 
+    def update_text(self):
+        self.count += 1
+        self.label.text = f"click count:{self.count}"
 
-menu_ui = Column(
-    0,
-    0,
-    spacing=12,
-    children=[
-        Button("Click", update_text, center_x=True),
-        label,
-    ],
-    w=pyxel.width,
-    h=pyxel.height,
-    align="center",
-)
+    def update(self):
+        self.column.update()
 
-
-def update():
-    menu_ui.update()
-
-
-def draw():
-    pyxel.cls(0)
-    menu_ui.draw()
+    def draw(self):
+        pyxel.cls(0)
+        self.column.draw()
 
 
-pyxel.run(update, draw)
+App()
