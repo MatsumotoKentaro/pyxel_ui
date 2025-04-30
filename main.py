10c830610ff7878ecbf1e92ca0c9119bf1454142
KentaroMatsumoto <matsumoto.kentaro@gmail.com>
2025-04-30 16:07:33 +0900

    Add Button

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pyxel
 
-from mini_ui import Label
+from mini_ui import Button, Label
 
 pyxel.init(160, 160, "Hello Pyxel")
 
 label = Label("Hello Pyxel", 10, 10, 7)
-label.draw()
+count = 0
 
-pyxel.show()
+
+def update_text():
+    global label, count
+    count += 1
+    label.text = f"Pressed:{count}"
+
+
+button = Button("Hello, Button", update_text, 10, 50, 100, 16, 1, 2)
+
+
+def update():
+    button.update()
+
+
+def draw():
+    pyxel.cls(0)
+    label.draw()
+    button.draw()
+
+
+pyxel.run(update, draw)
--- mini_ui.py
+++ mini_ui.py
@@ -19,3 +19,30 @@ class Label(Widget):
 
     def draw(self):
         pyxel.text(self.x, self.y, self.text, self.color)
+
+
+# ボタン（矩形＋テキスト＋クリック）
+class Button(Widget):
+    def __init__(
+        self, text, on_pressed, x=0, y=0, w=50, h=16, color_text=1, color_rect=1
+    ):
+        self.text = text
+        self.on_pressed = on_pressed
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color_text = color_text
+        self.color_rect = color_rect
+
+    def update(self):
+        if (
+            pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT)
+            and self.x <= pyxel.mouse_x <= self.x + self.w
+            and self.y <= pyxel.mouse_y <= self.y + self.h
+        ):
+            self.on_pressed()
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, self.color_rect)
+        pyxel.text(self.x + 4, self.y + 4, self.text, 0)
