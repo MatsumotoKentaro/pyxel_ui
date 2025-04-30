6304ef778ead3884c9b183fb198fb88543d6bfca
MatsumotoKentaro <matsumoto.kentaro@gmail.com>
2025-04-30 21:15:16 +0900

    1 基本的なUI部品の実装 (#2)
    * Add Label Widget
    
    * Add Button
    
    * Add Column
    
    * Update: 中央揃えできるように修正
    
    * Add .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 import pyxel
 
+from mini_ui import Button, Column, Label
+
 pyxel.init(160, 160, "Hello Pyxel")
+pyxel.mouse(True)
 
-pyxel.text(10, 10, "Hello Pyxel!", 10)
+count = 0
+label_text = f"click count:{count}"
+label = Label(label_text, color=7)
 
-pyxel.show()
+
+def update_text():
+    global count, label
+    count += 1
+    label.text = f"click count:{count}"
+
+
+menu_ui = Column(
+    0,
+    0,
+    spacing=12,
+    children=[
+        Button("Click", update_text, center_x=True),
+        label,
+    ],
+    w=pyxel.width,
+    h=pyxel.height,
+    align="center",
+)
+
+
+def update():
+    menu_ui.update()
+
+
+def draw():
+    pyxel.cls(0)
+    menu_ui.draw()
+
+
+pyxel.run(update, draw)
