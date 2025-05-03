6f8e2ee206c9e6c19f29a381d460c98fabc1f177
MatsumotoKentaro <matsumoto.kentaro@gmail.com>
2025-05-03 21:12:19 +0900

    Update: パッケージ化 (#8)

---
+++ mini_ui/__init__.py
@@ -0,0 +1,15 @@
+from .localstorage import clear, load, remove, save
+from .mini_ui import Blank, Button, Column, Label, TransButton, Widget
+
+__all__ = [
+    "Widget",
+    "Label",
+    "Button",
+    "TransButton",
+    "Blank",
+    "Column",
+    "save",
+    "load",
+    "remove",
+    "clear",
+]
